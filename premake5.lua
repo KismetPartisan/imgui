541ade15ebb1598008d3870568fa1191b1d9c46a
KismetPartisan <kismetpartisan@protonmail.com>
2022-10-08 19:07:58 -0400

    Updated premake file to support linux builds

--- premake5.lua
+++ premake5.lua
@@ -1,32 +1,22 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("obj/" .. outputdir .. "/%{prj.name}")
 
-	files {
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-        	"imgui_tables.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
+	files { "../*.h", "../*.cpp" }
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
 
 	filter "system:windows"
 		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
